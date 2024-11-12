a3c3ffcaf1f6bcb7a22c7b653f1f6d7a72188f74
Samarpan Harit <haritsamarpan7@gmail.com>
2024-11-12 23:37:59 +0530

    Pick django secret key from env variables

--- todo_project/settings/base.py
+++ todo_project/settings/base.py
@@ -21,8 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-w$a*-^hjqf&snr6qd&jkcq%0*5twb!_)qe0&z(2y-17umjr5tn"
-
+SECRET_KEY = os.getenv(
+    "DJANGO_SECRET_KEY",
+    "django-insecure-w$a*-^hjqf&snr6qd&jkcq%0*5twb!_)qe0&z(2y-17umjr5tn",
+)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 

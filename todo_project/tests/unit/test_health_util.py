84f9df1149a47c06b54f0eb675e4ca99f4572bb0
Samarpan Harit <haritsamarpan7@gmail.com>
2024-11-15 00:23:37 +0530

    Add success case for db health check util

--- todo_project/tests/unit/test_health_util.py
+++ todo_project/tests/unit/test_health_util.py
@@ -13,6 +13,21 @@ class DatabaseConfigTests(TestCase):
         self.db_client_patcher.stop()
 
     @patch("todo_project.utils.health.get_database_client")
+    def test_db_health_check_when_connection_successful(self, mock_get_database_client):
+        # Mocking the success scenario
+        mock_client = MagicMock()
+        mock_client.admin.command.return_value = {"ok": 1}
+        mock_get_database_client.return_value = mock_client
+
+        # Call the function under test
+        result = is_db_healthy()
+
+        # Assertions for success scenario
+        self.assertTrue(result)
+        mock_get_database_client.assert_called_once()
+        mock_client.admin.command.assert_called_once_with("ping")
+
+    @patch("todo_project.utils.health.get_database_client")
     def test_db_health_check_when_connection_failure(self, mock_get_database_client):
         mock_client = MagicMock()
         mock_client.admin.command.side_effect = ConnectionFailure("Mocked connection failure")
